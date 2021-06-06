56fc941669f227cfe08c3fea289119ac6cfce95c
danibr1 <75913820+danibr1@users.noreply.github.com>
2021-06-06 19:50:57 +0200

    completed

--- index.php
+++ index.php
@@ -24,9 +24,29 @@ class Movie {
     }
 
     // Metodi
-    public function getRegista() {
-        return $this->regista;
+    public function getDataUscita($data_uscita) {
+        return $data_uscita;
+    }
+
+    public function getDescrizione($descrizione) {
+        return $descrizione;
     }
 }
 
 // Istanza 1
+$interstellar = new Movie ('Interstellar','Christopher Nolan','2h49m','Avventura');
+echo '<h1>Titolo: ' . $interstellar->titolo . '</h1>';
+echo '<h3>Regista: ' . $interstellar->regista . '</h3>';
+echo '<h3>Durata: ' . $interstellar->durata . '</h3>';
+echo '<h3>Genere: ' . $interstellar->genere . '</h3>';
+echo '<h3>Data Uscita: ' . $interstellar->getDataUscita('26 ottobre 2014') . '</h3>';
+echo '<h4>Descrizione: ' . $interstellar->getDescrizione('In un futuro non precisato, un drastico cambiamento climatico colpisce duramente l\'agricoltura. Il granturco è l\'unica coltivazione ancora in grado di crescere ed un gruppo di scienziati è intenzionato ad attraversare lo spazio per trovare nuovi luoghi adatti a coltivarlo.') . '</h4><br>';
+
+// Istanza 2
+$gravity = new Movie ('Gravity','Alfonso Cuarón','1h31m','Fantascienza');
+echo '<h1>Titolo: ' . $gravity->titolo . '</h1>';
+echo '<h3>Regista: ' . $gravity->regista . '</h3>';
+echo '<h3>Durata: ' . $gravity->durata . '</h3>';
+echo '<h3>Genere: ' . $gravity->genere . '</h3>';
+echo '<h3>Data Uscita: ' . $gravity->getDataUscita('03 ottobre 2013') . '</h3>';
+echo '<h4>Descrizione: ' . $gravity->getDescrizione('In missione a bordo dell\'Explorer, l\'astronauta Matt Kowalski e la dottoressa Ryan Stone, esperta di ingegneria medica, cercano di sopravvivere alla distruzione della navicella sulla quale si trovavano.') . '</h4><br>';
